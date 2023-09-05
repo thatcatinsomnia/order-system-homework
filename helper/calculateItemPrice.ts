25039c6993d3835432d2b1626a4f62863533dd50
Bennett <bennett@null.com>
2023-09-05 14:28:22 +0800

    fix: fix incorrect total price in ItemDetail
    - add condition that price is not equal -1

--- helper/calculateItemPrice.ts
+++ helper/calculateItemPrice.ts
@@ -1,15 +1,20 @@
 import type { PickedItem } from '../src/stores/usePickedItemStore';
 import type { OrderItem } from '../src/stores/useShoppingCartStore';
 
+// free price is used to display text "FREE" when price is -1
+// do not calculate -1 in the total price
+// this is just for test purpose
+const FREE_PRICE = -1;
+
 export default function calculateItemPrice(item: PickedItem | OrderItem) {
   if (!item.item) {
     return 0;
   }
-  
+
   const basePrice = item.item.price;
 
   const variantsTotalPrice = item.item.variants.reduce((total, variant) => {
-    if (variant.type === 'radio') {
+    if (variant.type === 'radio' && variant.price !== FREE_PRICE) {
       total += variant.price;
     }
 
